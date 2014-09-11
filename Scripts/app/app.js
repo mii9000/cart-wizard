c619be14e4f0ab6e1f84b779294d913d7b32e719
Ibrahim-Islam <ibrahim.islam@live.com>
2014-09-11 18:51:32 +0600

    Remove unnecessary files

---
+++ Scripts/app/app.js
@@ -0,0 +1,47 @@
+﻿/// <reference path="../libs/angular.js" />
+
+'use strict';
+
+angular.module('cart-wizard', ['ui.router', 'cart-widget.controllers', 'ui.bootstrap', 'ngAnimate'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+        .state('product', {
+            url: '/',
+            templateUrl: 'partials/home.html',
+            controller: 'ProductController'
+        })
+        .state('cart', {
+            url: '/',
+            templateUrl: 'partials/cart.html',
+            controller: 'CartController'
+        })
+        .state('login', {
+            url: '/',
+            templateUrl: 'partials/login.html',
+            controller: 'LoginController'
+        })
+        .state('card', {
+            url: '/',
+            templateUrl: 'partials/card.html',
+            controller: 'CardController'
+        });
+
+        $urlRouterProvider.otherwise('/');
+    })
+    .factory('Storage', function () {
+        var order = { };
+        return {
+            SetProduct: function (product) {
+                order.Product = product;
+            },
+            SetUser: function (user) {
+                order.User = user;
+            },
+            SetCard: function (card) {
+                order.Card = card;
+            },
+            GetOrder: function () {
+                return order;
+            }
+        }
+    });
---
+++ Scripts/app/controller.js
@@ -0,0 +1,70 @@
+﻿/// <reference path="../libs/angular.js" />
+
+'use strict';
+
+angular.module('cart-widget.controllers', [])
+    .controller('DataController', ['$scope', 'Storage', function ($scope, Storage) {
+        $scope.data = Storage.GetOrder();
+    }])
+    .controller('ProductController', ['$scope', '$http', '$state', 'Storage', function ($scope, $http, $state, Storage) {
+        $scope.CompanyName = '';
+        $scope.Products = [];
+        $scope.ProductItems = [];
+
+        $http.get('data/products.json').success(function (responsedata) {
+            //Demonstrate handling response data to fit View structure 
+            for (var i = 0; i < responsedata.Products.length; i++) {
+                responsedata.Products[i].ProductQuantity = 1;
+            }
+
+            $scope.CompanyName = responsedata.CompanyName;
+            $scope.Products = responsedata.Products;
+
+            var firstProduct = $scope.Products[0];
+            for (var i = 0; i < firstProduct.ProductItems.length; i++) {
+                $scope.ProductItems.push({ ItemName: firstProduct.ProductItems[i].ProductItemName });
+            }
+
+        });
+
+        $scope.select = function (product) {
+            Storage.SetProduct(product);
+            $state.go('cart');
+        };
+    }])
+    .controller('CartController', ['$scope', '$state', 'Storage', function ($scope, $state, Storage) {
+        $scope.Product = Storage.GetOrder().Product;
+
+        //Demonstrate returning values from controller
+        $scope.Total = function () {
+            return $scope.Product.ProductPrice * $scope.Product.ProductQuantity;
+        };
+    }])
+    .controller('LoginController', ['$scope', '$state', 'Storage', function ($scope, $state, Storage) {
+        $scope.user = { email: '', password: ''};
+
+        $scope.Login = function (isvalid) {
+            if (isvalid) {
+                Storage.SetUser($scope.user);
+                $state.go('card');
+            }
+        };
+    }])
+    .controller('CardController', ['$scope', '$state', 'Storage', '$modal', function ($scope, $state, Storage, $modal) {
+        $scope.IsPaying = false;
+
+        $scope.Pay = function (card) {
+            $scope.IsPaying = true;
+            Storage.SetCard(card);
+
+            //Simulate working on server-side
+            setTimeout(function () {
+                $scope.IsPaying = false;
+
+                //Demonstrate Angular.ui.bootstrap
+                $modal.open({
+                    template: '<div class="modal-body"><h4>Congratulations! You just bought a car!</h4></div><div class="modal-footer"><button class="btn btn-primary" ng-click="$parent.$close()">OK</button></div>'
+                });
+            }, 2000);
+        };
+    }]);
